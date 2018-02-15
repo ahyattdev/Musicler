017b6d1fbe02b93b03ca2935836ec37db397fcee
Andrew Hyatt <ahyattdev@icloud.com>
2018-02-15 12:21:24 -0500

    Initial Commit

---
+++ Musicler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Musicler
+//
+//  Created by Andrew Hyatt on 2/15/18.
+//  Copyright © 2018 Andrew Hyatt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
